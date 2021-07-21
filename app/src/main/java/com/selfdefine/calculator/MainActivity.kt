1312bc589ca1407ba8e66c9e068325b332e3007b
budhirajamadhav <madhavbudhiraja131@gmail.com>
2021-07-21 13:57:05 +0530

    Added Operation Functions

--- app/src/main/java/com/selfdefine/calculator/MainActivity.kt
+++ app/src/main/java/com/selfdefine/calculator/MainActivity.kt
@@ -8,7 +8,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 
     fun onRadioButtonClicked(view: View) {}
+
+    fun addition(operand1: Long, operand2: Long): String {
+        return (operand1 + operand2).toString()
+    }
+    fun subtraction(operand1: Long, operand2: Long): String {
+        return (operand1 - operand2).toString()
+    }
+    fun multiplication(operand1: Long, operand2: Long): String {
+        return (operand1 * operand2).toString()
+    }
+    fun division(operand1: Long, operand2: Long): String {
+        if(operand2 == 0L) return "∞"
+        return (operand1 / operand2).toString()
+    }
+
 }
