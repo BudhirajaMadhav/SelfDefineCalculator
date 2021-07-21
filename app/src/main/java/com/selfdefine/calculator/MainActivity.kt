2b38cf7e071758026921d439cd8fd2d9c3c35a6f
rahulsain <50570677+rahulsain@users.noreply.github.com>
2021-07-21 17:17:42 +0530

    perform basic operation by data binding and calculate()

--- app/src/main/java/com/selfdefine/calculator/MainActivity.kt
+++ app/src/main/java/com/selfdefine/calculator/MainActivity.kt
@@ -1,31 +1,92 @@
 package com.selfdefine.calculator
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.selfdefine.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var index: Int = -1
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-
+        binding.calculate.setOnClickListener { calculate() }
     }
 
-    fun onRadioButtonClicked(view: View) {}
+    private fun calculate() {
+        val number1 = binding.firstInput.text.toString()
+        val number2 = binding.secondInput.text.toString()
 
-    fun addition(operand1: Long, operand2: Long): String {
+        val operatorArr = arrayOf("+", "-", "*", "/")
+        val id = binding.radioGroup.checkedRadioButtonId
+//        var index = -1
+
+        var firstNumberL = 0L
+        var secondNumberL = 0L
+
+        if (number1 != "") {
+            firstNumberL = number1.toLong()
+        }
+        if (number2 != "") {
+            secondNumberL = number2.toLong()
+        }
+
+        val result = when (id) {
+            R.id.radio_add -> addition(firstNumberL, secondNumberL)
+            R.id.radio_sub -> subtraction(firstNumberL, secondNumberL)
+            R.id.radio_mul -> multiplication(firstNumberL, secondNumberL)
+            R.id.radio_div -> division(firstNumberL, secondNumberL)
+            else -> "z"
+        }
+
+        var operand1 = "x"
+        var operand2 = "y"
+        var operator = "invalid"
+        if (index != -1) {
+            operator = operatorArr[index]
+        }
+        if (number1 != "") {
+            operand1 = number1
+        }
+        if (number2 != "") {
+            operand2 = number2
+        }
+
+        val intent = Intent(this, ResultActivity::class.java)
+
+        intent.putExtra("OPERAND_1", operand1)
+        intent.putExtra("OPERAND_2", operand2)
+        intent.putExtra("OPERATOR", operator)
+        intent.putExtra("RESULT", result)
+
+        startActivity(intent)
+    }
+
+    private fun addition(operand1: Long, operand2: Long): String {
+        index = 0
         return (operand1 + operand2).toString()
     }
-    fun subtraction(operand1: Long, operand2: Long): String {
+
+    private fun subtraction(operand1: Long, operand2: Long): String {
+        index = 1
         return (operand1 - operand2).toString()
     }
-    fun multiplication(operand1: Long, operand2: Long): String {
+
+    private fun multiplication(operand1: Long, operand2: Long): String {
+        index = 2
         return (operand1 * operand2).toString()
     }
-    fun division(operand1: Long, operand2: Long): String {
-        if(operand2 == 0L) return "∞"
-        return (operand1 / operand2).toString()
+
+    private fun division(operand1: Long, operand2: Long): String {
+        index = 3
+        if (operand2 == 0L) return "∞"
+        return (operand1 / operand2.toDouble()).toString()
     }
 
 }
--- app/src/main/java/com/selfdefine/calculator/ResultActivity.kt
+++ app/src/main/java/com/selfdefine/calculator/ResultActivity.kt
@@ -1,23 +1,23 @@
 package com.selfdefine.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        val operand1 = intent.getLongExtra("OPERAND_1", 0)
-        val operand2 = intent.getLongExtra("OPERAND_2", 0)
+        val operand1 = intent.getStringExtra("OPERAND_1")
+        val operand2 = intent.getStringExtra("OPERAND_2")
         val operator = intent.getStringExtra("OPERATOR")
-        val result = intent.getLongExtra("RESULT", 0)
+        val result = intent.getStringExtra("RESULT")
 
         val questionView: TextView = findViewById(R.id.question)
         val resultView: TextView = findViewById(R.id.result)
 
-        questionView.text = "$operand1 $operator $operand2 ="
-        resultView.text = "$result"
+        questionView.text = getString(R.string.question_view, operand1, operator, operand2)
+        resultView.text = result
     }
 }
